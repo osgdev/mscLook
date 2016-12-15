c04ab40ad0a0d5b6200dbace28b4dcce0722be84
dendlel <leon.dendle@dvla.gsi.gov.uk>
2016-12-15 08:26:20 +0000

    Changed args array, added logging

--- MscLookup/src/uk/gov/dvla/osg/msclookup/Main.java
+++ MscLookup/src/uk/gov/dvla/osg/msclookup/Main.java
@@ -1,18 +1,20 @@
 package uk.gov.dvla.osg.msclookup;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
-import uk.gov.dvla.osg.msclookup.implementations.RmMscLookup;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 import uk.gov.dvla.osg.msclookup.interfaces.LookupMsc;
 
 import com.google.inject.Guice;
 import com.google.inject.Injector;
-
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 
 public class Main {
 	
@@ -21,17 +23,38 @@ public class Main {
 	public static void main(String[] args) {
 
 		LOGGER.info("Starting uk.gov.dvla.osg.msclookup.Main");
+		LOGGER.debug("{} args passed ",args.length);
 		String input = "";
 		String output = "";
+		String delim = "";
+		int position = 0;
+		int noOfZeros = 0;
 		
-		if(args.length == 0){
-			input = "C:\\Users\\dendlel\\Desktop\\DataMakerResources\\OTHER.TEST.1.DAT";
-			output = "C:\\Users\\dendlel\\Desktop\\DataMakerResources\\UPDATED.DPF.DAT";
+		if(args.length == 5){
+			try{
+				input = args[0];
+				output = args[1];
+				delim = args[2];
+				position = Integer.parseInt(args[3]);
+				noOfZeros = Integer.parseInt(args[4]);
+			}catch (NumberFormatException e){
+				LOGGER.fatal(e.getMessage());
+				System.exit(1);
+			}
 		}else{
-			input = args[0];
-			output = args[1];
+			LOGGER.fatal("Incorrect number of args ({}) passed to app. "
+					+ "Required args are: input file, output file, input"
+					+ " file delimitter, postcode position (base0), number "
+					+ "of zeros in result", args.length);
+			System.exit(1);
 		}
 
+		LOGGER.debug("Input file set to '{}'",input);
+		LOGGER.debug("Output file set to '{}'",output);
+		LOGGER.debug("Delimitter set to '{}'",delim);
+		LOGGER.debug("Position set to '{}'",position);
+		LOGGER.debug("NoOfZeros set to '{}'",noOfZeros);
+		
 		//Build container
 		Injector injector = Guice.createInjector(new ApplicationInjector());        
 		//Use container
@@ -40,14 +63,24 @@ public class Main {
 		try {
 			String line;
 			String[] parts;
+			String result;
 			FileInputStream fis = new FileInputStream(input);
 			BufferedReader br = new BufferedReader(new InputStreamReader(fis));
+			
 			while ( (line = br.readLine()) != null ){
-				parts=line.split(",");
-				LOGGER.trace("MSC = '{}'", lmsc.getMsc(parts[10], 2));
+				parts=line.split(delim);
+				result=lmsc.getMsc(parts[position], noOfZeros);
+				LOGGER.trace("MSC = '{}'",result);
+				try (BufferedWriter writer = Files.newBufferedWriter(Paths.get(output))) {
+		            writer.write(line + "," + result + "\n");
+		        } catch (IOException e){
+		        	LOGGER.fatal(e.getMessage());
+					System.exit(1);
+		        }
 			}
 		} catch (IOException e) {
 			LOGGER.fatal(e.getMessage());
+			System.exit(1);
 		}
 		
 	}
